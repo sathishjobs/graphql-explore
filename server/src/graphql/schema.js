d31e6f63fd3b455128721ff2c99e91dcff61eb75
sathish-steve <sathishkumarcisco@gmail.com>
2018-05-07 14:47:33 +0530

    initial server setup 0

---
+++ server/src/graphql/schema.js
@@ -0,0 +1,21 @@
+export default `
+    type Tweet {
+        _id : ID!,
+        text : String
+    }
+
+    type Query {
+        getTweet(_id : ID!) :Tweet
+        getTweets : [Tweet]
+    }
+
+    
+    type Mutation{
+         createTweet(text:String!):Tweet
+    }
+
+    schema {
+        query : Query,
+        mutation : Mutation,
+    }
+`
---
+++ server/src/mocks/index.js
@@ -0,0 +1,20 @@
+import faker from 'faker';
+
+import Tweet from "../models/Tweet";
+
+const TWEETS_TOTAL = 10;
+
+export default async () =>{
+    try {
+        await Tweet.remove({});
+        await Array.from({
+            length : TWEETS_TOTAL
+        }).forEach(async ()=>{
+            return await Tweet.create({
+                text : faker.lorem.paragraphs(1)
+            })
+        })
+    } catch (error) {
+        throw error;
+    }
+}
